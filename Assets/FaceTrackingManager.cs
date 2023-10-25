dc86e588f7441ed34b92d923ac9c16c716ce1a36
MehdiDaemi <53620014+MehdiDaemi@users.noreply.github.com>
2023-10-25 16:01:12 +0200

    wrong facetracking maanger was picked

--- Assets/FaceTrackingManager.cs
+++ Assets/FaceTrackingManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -6,61 +7,66 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SmileState
-{
-    public bool smiling;
-    public bool slightSmile;
-    public bool pucker;
-    public bool slightPucker;
-}
-
 public class FaceTrackingManager : MonoBehaviour
 {
     [SerializeField] private OVRFaceExpressions faceExpressions;
-     
+
     [Range(-0.05f, 0.05f)]
     [SerializeField] private float _mouthValue; //from -1 to +1
 
     [SerializeField] private float _puckerThreshold;
+    [SerializeField] private float _neutralThreshold;
     [SerializeField] private float _smileThreshold;
 
-    [SerializeField] private bool _smiling;
-    [SerializeField] private bool _slightSmile;
-    [SerializeField] private bool _pucker;
-    [SerializeField] private bool _slightPucker;
-    
+    private bool _smiling;
+    private bool _slightSmile;
+    private bool _pucker;
+    private bool _slightPucker;
+
+    [SerializeField] private FaceData _faceData;
+
     [SerializeField] private bool _manualSmileControl;
-    
+
+    [SerializeField] private bool _autoDebugBreathing;
+    [SerializeField] private float _debugBreathRate;
+
     public delegate void OnMouthValue(float mouthValue);
     public static OnMouthValue MouthValue;
-    
-    public delegate void OnPucker();
-    public static OnPucker Pucker;
-    
-    public delegate void OnSmile();
-    public static OnSmile Smile;
 
-    public delegate void OnSlightSmile();
-    public static OnSlightSmile SlightSmile;
+    public delegate void OnFaceExpression();
+    public static OnFaceExpression FaceExpression;
 
-    public delegate void OnSlightPucker();
-    public static OnSlightPucker SlightPucker;
-    
     private int _previousProgressValue;
     private int _progressValue;
 
     private void Update()
     {
+        float smileValue = 0f;
+        float puckerValue = 0f;
+
         Vector2 lipPucker = new Vector2();
         Vector2 lipCornerPuller = new Vector2();
 
         lipPucker = GetExpressionValue(OVRFaceExpressions.FaceExpression.LipPuckerL, OVRFaceExpressions.FaceExpression.LipPuckerR);
         lipCornerPuller = GetExpressionValue(OVRFaceExpressions.FaceExpression.LipCornerPullerL, OVRFaceExpressions.FaceExpression.LipCornerPullerR);
 
-        float smileValue = 0f;
-        float puckerValue = 0f;
-        
-        if (!_manualSmileControl)
+        if (_autoDebugBreathing)
+        {
+            _mouthValue = Mathf.Sin(Time.time * _debugBreathRate) * 0.075f;
+            //pucker value is the negative half of mouthvalue
+            if (_mouthValue < 0)
+            {
+                puckerValue = -_mouthValue;
+                smileValue = 0f;
+            }
+            //smile value is the positive half of mouthvalue
+            else if (_mouthValue > 0)
+            {
+                puckerValue = 0f;
+                smileValue = _mouthValue;
+            }
+        }
+        else if (!_manualSmileControl)
         {
             smileValue = (lipCornerPuller.x + lipCornerPuller.y) / 2;
             puckerValue = (lipPucker.x + lipPucker.y) / 2;
@@ -70,7 +76,7 @@ public class FaceTrackingManager : MonoBehaviour
         {
             if (_mouthValue < 0)
             {
-                puckerValue = - _mouthValue;
+                puckerValue = -_mouthValue;
                 smileValue = 0;
             }
             else if (_mouthValue > 0)
@@ -79,29 +85,39 @@ public class FaceTrackingManager : MonoBehaviour
                 puckerValue = 0;
             }
         }
-        
-        //MouthValue(_mouthValue);
-        
-        bool wasSlightSmile = _slightSmile;
-        bool wasSlightPucker = _slightPucker;
-        bool wasSmile = _smiling;
-        bool wasPucker = _pucker;
-        
-        _smiling = smileValue >= _smileThreshold;
-        _slightSmile = smileValue < _smileThreshold && _mouthValue > 0;
-        _pucker = puckerValue >= _puckerThreshold;
-        _slightPucker = puckerValue < _puckerThreshold && _mouthValue < 0;
 
-        if (!wasPucker && _pucker)
+        MouthValue(_mouthValue);
+
+        _smiling = smileValue >= _smileThreshold;
+        _slightSmile = smileValue < _smileThreshold && _mouthValue > _neutralThreshold;
+        _pucker = puckerValue >= _puckerThreshold;
+        _slightPucker = puckerValue < _puckerThreshold && _mouthValue < _neutralThreshold;
+
+        if (!_faceData.previouslyPucker && _pucker)
         {
-            Pucker();
-            UnityEngine.Debug.Log("HERE");
+            _faceData.SetData(false, false, false, true);
+            FaceExpression();
         }
-        if (!wasSmile && _smiling) Smile();
-        if (!wasSlightPucker && _slightPucker) SlightPucker();
-        if (!wasSlightSmile && _slightSmile) SlightSmile();
+
+        else if (!_faceData.previouslySmiling && _smiling)
+        {
+            _faceData.SetData(true, false, false, false);
+            FaceExpression();
+        }
+
+        else if (!_faceData.previouslySlightPucker && _slightPucker)
+        {
+            _faceData.SetData(false, false, true, false);
+            FaceExpression();
+        }
+
+        else if (!_faceData.previouslySlightSmile && _slightSmile)
+        {
+            _faceData.SetData(false, true, false, false);
+            FaceExpression();
+        }
     }
-    
+
     private Vector2 GetExpressionValue(OVRFaceExpressions.FaceExpression key1,
         OVRFaceExpressions.FaceExpression key2)
     {
