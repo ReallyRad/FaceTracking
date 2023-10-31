7f50f1c698cb731e9806468e4e2601ac06cbb3e0
ReallyRad <arthur.pointeau@gmail.com>
2023-10-31 13:57:40 +0100

    remove unused coe

--- Assets/MusicManager.cs
+++ Assets/MusicManager.cs
@@ -1,38 +1,9 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using Metaface.Debug;
 
 public class MusicManager : MonoBehaviour
 {
     private void OnEnable()
     {
-        if (ModeControl.Discrete)
-        {
-            if (ModeControl.auraApproachingRP == "Progressive")
-            {
-                if (ModeControl.DiscreteFB_In_Ex)
-                {
-                    BreathingControl.New_In_Ex_FullBreath += DiscreteProgressiveWhole;
-                }
-                if (ModeControl.DiscreteFB_PeInPi_Ex)
-                {
-                    BreathingControl.New_PeInPi_Ex_FullBreath += DiscreteProgressiveWhole;
-                }
-            }
-            if (ModeControl.auraApproachingRP == "Interactive")
-            {
-                if (ModeControl.DiscreteFB_In_Ex)
-                {
-                    BreathingControl.New_In_Ex_FullBreath += DiscreteInteractiveWhole;
-                }
-                if (ModeControl.DiscreteFB_PeInPi_Ex)
-                {
-                    BreathingControl.New_PeInPi_Ex_FullBreath += DiscreteInteractiveWhole;
-                }
-            }
-        }
         if (ModeControl.Continuous)
         {
             if (ModeControl.auraApproachingRP == "Progressive")
@@ -50,50 +21,10 @@ public class MusicManager : MonoBehaviour
                     BreathingControl.New_PeInPi_LongerThanMinimum = ContinuousProgressiveWhole;
                 }
             }
-            if (ModeControl.auraApproachingRP == "Interactive")
-            {
-                if (ModeControl.ContinuousACT_Ex)
-                {
-                    BreathingControl.New_Ex_LongerThanMinimum += ContinuousInteractiveWhole;
-                }
-                if (ModeControl.ContinuousACT_In)
-                {
-                    BreathingControl.New_In_LongerThanMinimum += ContinuousInteractiveWhole;
-                }
-                if (ModeControl.ContinuousACT_PeInPi)
-                {
-                    BreathingControl.New_PeInPi_LongerThanMinimum += ContinuousInteractiveWhole;
-                }
-            }
         }
     }
     private void OnDisable()
     {
-        if (ModeControl.Discrete)
-        {
-            if (ModeControl.auraApproachingRP == "Progressive")
-            {
-                if (ModeControl.DiscreteFB_In_Ex)
-                {
-                    BreathingControl.New_In_Ex_FullBreath -= DiscreteProgressiveWhole;
-                }
-                if (ModeControl.DiscreteFB_PeInPi_Ex)
-                {
-                    BreathingControl.New_PeInPi_Ex_FullBreath -= DiscreteProgressiveWhole;
-                }
-            }
-            if (ModeControl.auraApproachingRP == "Interactive")
-            {
-                if (ModeControl.DiscreteFB_In_Ex)
-                {
-                    BreathingControl.New_In_Ex_FullBreath -= DiscreteInteractiveWhole;
-                }
-                if (ModeControl.DiscreteFB_PeInPi_Ex)
-                {
-                    BreathingControl.New_PeInPi_Ex_FullBreath -= DiscreteInteractiveWhole;
-                }
-            }
-        }
         if (ModeControl.Continuous)
         {
             if (ModeControl.auraApproachingRP == "Progressive")
@@ -111,115 +42,36 @@ public class MusicManager : MonoBehaviour
                     BreathingControl.New_PeInPi_LongerThanMinimum -= ContinuousProgressiveWhole;
                 }
             }
-            if (ModeControl.auraApproachingRP == "Interactive")
-            {
-                if (ModeControl.ContinuousACT_Ex)
-                {
-                    BreathingControl.New_Ex_LongerThanMinimum -= ContinuousInteractiveWhole;
-                }
-                if (ModeControl.ContinuousACT_In)
-                {
-                    BreathingControl.New_In_LongerThanMinimum -= ContinuousInteractiveWhole;
-                }
-                if (ModeControl.ContinuousACT_PeInPi)
-                {
-                    BreathingControl.New_PeInPi_LongerThanMinimum -= ContinuousInteractiveWhole;
-                }
-            }
         }
     }
 
-    private float maxVolume = 1f;
-    private float currentVolumeOfCurrentSL = 0;
-    private float currentVolumeOfCurrentSLIncrease = 0;
+    private float _maxVolume = 1f;
+    private float _currentVolumeOfCurrentSL;
+    private float _currentVolumeOfCurrentSLIncrease;
+    
     [SerializeField] private SeamlessLoop[] _seamlessLoops;
     [SerializeField] private int _progress;
     [SerializeField] private int _currentLoopIndex;
-
-    private void DiscreteProgressiveWhole()
-    {
-        Vector3 movement = AllVfxsControl.auraApproachingFinalPosition - AllVfxsControl.auraApproachingInitialPosition;
-        float movementSteps = AllVfxsControl.auraApproachingEndBN - AllVfxsControl.auraApproachingStartBN;
-        Vector3 movementSpeed = movement / movementSteps;
-
-        if (gameObject.activeSelf)
-        {
-            StartCoroutine(DiscreteProgressiveIncrement(AllVfxsControl.progressIncrementTimeForDiscrete, movementSpeed));
-        }
-    }
-    private IEnumerator DiscreteProgressiveIncrement(float increment, Vector3 speed)
-    {
-        float startTime = Time.time;
-        float endTime = startTime + increment;
-
-        while (Time.time < endTime)
-        {
-            //ps.transform.position += (speed / increment) * Time.deltaTime;
-            yield return null;
-        }
-    }
-
-    private void DiscreteInteractiveWhole()
-    {
-        if (gameObject.activeSelf)
-        {
-            StartCoroutine(DiscreteInteractiveIncrement(AllVfxsControl.interactiveLoopTimeForDiscrete,
-                                                        AllVfxsControl.auraApproachingInitialPosition,
-                                                        AllVfxsControl.auraApproachingFinalPosition));
-        }
-    }
-    private IEnumerator DiscreteInteractiveIncrement(float increment, Vector3 initialPosition, Vector3 finalPosition)
-    {
-        float startTime = Time.time;
-        float middleTime = startTime + (increment / 2);
-        float endTime = startTime + increment;
-
-        while (Time.time < middleTime)
-        {
-            float progress = (middleTime - Time.time) / (increment / 2);
-            Vector3 currentPosition = Vector3.Lerp(initialPosition, finalPosition, progress);
-            //ps.transform.position = currentPosition;
-            yield return null;
-        }
-        while (Time.time > middleTime && Time.time < endTime)
-        {
-            float progress = (endTime - Time.time) / (increment / 2);
-            Vector3 currentPosition = Vector3.Lerp(finalPosition, initialPosition, progress);
-            //ps.transform.position = currentPosition;
-            yield return null;
-        }
-    }
 
     private void ContinuousProgressiveWhole(float fraction)
     {
         if (_currentLoopIndex < _seamlessLoops.Length)
         {
-            currentVolumeOfCurrentSL = _seamlessLoops[_currentLoopIndex].GetVolume();
-            if (currentVolumeOfCurrentSL < maxVolume)
+            _currentVolumeOfCurrentSL = _seamlessLoops[_currentLoopIndex].GetVolume();
+            if (_currentVolumeOfCurrentSL < _maxVolume)
             {
-                currentVolumeOfCurrentSLIncrease = maxVolume * fraction * Time.deltaTime;
-                _seamlessLoops[_currentLoopIndex].SetVolume(currentVolumeOfCurrentSL + currentVolumeOfCurrentSLIncrease);
+                _currentVolumeOfCurrentSLIncrease = _maxVolume * fraction * Time.deltaTime;
+                _seamlessLoops[_currentLoopIndex].SetVolume(_currentVolumeOfCurrentSL + _currentVolumeOfCurrentSLIncrease);
             }
             else if (_currentLoopIndex < (_seamlessLoops.Length - 1))
             {
                 _currentLoopIndex++;
-                currentVolumeOfCurrentSL = _seamlessLoops[_currentLoopIndex].GetVolume();
-                currentVolumeOfCurrentSLIncrease = maxVolume * fraction * Time.deltaTime;
-                _seamlessLoops[_currentLoopIndex].SetVolume(currentVolumeOfCurrentSL + currentVolumeOfCurrentSLIncrease);
+                _currentVolumeOfCurrentSL = _seamlessLoops[_currentLoopIndex].GetVolume();
+                _currentVolumeOfCurrentSLIncrease = _maxVolume * fraction * Time.deltaTime;
+                _seamlessLoops[_currentLoopIndex].SetVolume(_currentVolumeOfCurrentSL + _currentVolumeOfCurrentSLIncrease);
             }
         }
     }
-
-    private void ContinuousInteractiveWhole(float fraction)
-    {
-        Vector3 currentPosition = Vector3.Lerp(AllVfxsControl.auraApproachingInitialPosition,
-                                               AllVfxsControl.auraApproachingFinalPosition,
-                                               fraction);
-
-        //ps.transform.position = currentPosition;
-    }
-
-
 
     private void LevelUp()
     {
