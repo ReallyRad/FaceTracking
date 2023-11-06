c1a66cb2db84721962a52a7290bc744adebb6f88
Arthur Pointeau <arthur.pointeau@gmail.com>
2023-11-06 15:31:44 +0100

    add playback rate hack

--- Assets/Plugins/Calcatz/CookieCutter-Core/NodeBasedCommand/Main/ChangeTimeScaleCommand.cs
+++ Assets/Plugins/Calcatz/CookieCutter-Core/NodeBasedCommand/Main/ChangeTimeScaleCommand.cs
@@ -18,7 +18,8 @@ namespace Calcatz.CookieCutter {
         }
 
         public override void Execute(CommandExecutionFlow _flow) {
-            Time.timeScale = timeScale;
+            float val = GetInput<float>(_flow, 0, timeScale);
+            Time.timeScale = val;
             Exit();
         }
 
@@ -36,7 +37,12 @@ namespace Calcatz.CookieCutter {
         public override void Editor_OnDrawContents(Vector2 _absPosition) {
             base.Editor_OnDrawContents(_absPosition);
             CommandGUI.DrawInPoint(1);
-            CommandGUI.DrawFloatField("Time Scale", "Time scale value.", ref timeScale, 0);
+            if (inputIds[0].targetId == 0) {
+                CommandGUI.DrawFloatField("Time Scale", "Time scale value.", ref timeScale, 0);
+            }
+            else {
+                CommandGUI.DrawLabel("Time Scale");
+            }
         }
 #endif
     }
