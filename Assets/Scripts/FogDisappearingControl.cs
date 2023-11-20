a89c6426aab7c4c50d53859a60d92740689c4df2
ReallyRad <arthur.pointeau@gmail.com>
2023-11-20 17:37:30 +0100

    fog reacting to progress and disabling itslef

--- Assets/Scripts/FogDisappearingControl.cs
+++ Assets/Scripts/FogDisappearingControl.cs
@@ -9,19 +9,22 @@ public class FogDisappearingControl : Sequenceable
     [SerializeField] private VolumetricFog fog;
 
     [SerializeField] private float _initialValue; //TODO put it in the abstract class?
+    [SerializeField] private float _finalValue; //TODO put it in the abstract class?
+    [SerializeField] private float _completedProgressAt; //TODO put it in the abstract class?
     
     protected override void Progress(float progress) 
     {
         if (_active)
         {
-            if (progress <= _finalValue)
+            Debug.Log("fog progress " + progress);
+            if (progress >= _completedProgressAt)
             {
                 Completed(this);
                 _active = false;
             }
             else
             {
-                fog.settings.density = Utils.Map(progress, 0, 6, _initialValue, 0);
+                fog.settings.density = Utils.Map(progress, 0, _completedProgressAt, _initialValue, _finalValue);
             }
         }
     }
@@ -29,7 +32,7 @@ public class FogDisappearingControl : Sequenceable
     public override void Initialize()
     {
         _active = true;
-
+        fog.settings.density = _initialValue;
         /* 
          fog = VolumetricFog.instance;
          fog.fogAreaPosition = Vector3.zero;
