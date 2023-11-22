f8c7b5898ec2cd643c32cebc47723fcc52b10f18
ReallyRad <arthur.pointeau@gmail.com>
2023-11-22 17:54:40 +0100

    minor stuff

--- Assets/Scripts/ProgressManager.cs
+++ Assets/Scripts/ProgressManager.cs
@@ -42,7 +42,7 @@ using Debug = UnityEngine.Debug;
         if (_puckerStopwatch.ElapsedMilliseconds / 1000f > _startProgressAt && 
             _previousElapsed / 1000f < _startProgressAt) //we just passed the min duration threshold, continue progress
         {
-            _progressTween = LeanTween.value(gameObject, _currentProgress, _currentProgress + 1, 4)
+            _progressTween = LeanTween.value(gameObject, _currentProgress, _currentProgress + 1, _endProgressAt - _startProgressAt)
                 .setOnUpdate(val =>
                 {
                     _currentProgress = val;
@@ -64,15 +64,18 @@ using Debug = UnityEngine.Debug;
     {
         if (_faceExpression.slightSmile) //only reset stopwatch once we passed 0
         {
+            Debug.Log("Slight smile");
             if (_faceExpression.previouslySlightPucker) _puckerStopwatch.Reset(); 
         }
         else if (_faceExpression.slightPucker) //pause the tween. We might resume it if we detect pucker again
         {
+            Debug.Log("Slight pucker");
             if (Progressing()) LeanTween.pause(_progressTween); 
             _puckerStopwatch.Stop();
         }
         else if (_faceExpression.pucker) //if we resumed pucker while we were already doing progress
         {
+            Debug.Log("pucker");
             if (Progressing()) LeanTween.resume(_progressTween);
             _puckerStopwatch.Start();
         }
@@ -84,7 +87,7 @@ using Debug = UnityEngine.Debug;
                _puckerStopwatch.ElapsedMilliseconds / 1000 < _endProgressAt;
     }
 
-    private void ResetProgress(ProgressiveSequenceable item)
+    private void ResetProgress(Sequenceable item)
     {
         LeanTween.pause(_progressTween); //TODO is it necessary to pause?
         _currentProgress = 0;
--- Assets/Scripts/Sequenceable.cs
+++ Assets/Scripts/Sequenceable.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public abstract class Sequenceable : MonoBehaviour
 {
-    public delegate void OnCompleted(ProgressiveSequenceable item); //triggered to notify the current element is done
+    public delegate void OnCompleted(Sequenceable item); //triggered to notify the current element is done
     public static OnCompleted Completed;
     
     [SerializeField] protected bool _active; //whether the item is currently receiving progress updates
     [SerializeField] protected float _completedProgressAt;
 
     public abstract void Initialize(); //Initialize the values before starting to interpolate.
-
 }
+    
