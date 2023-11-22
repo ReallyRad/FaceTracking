3acbf23aee6598fe73a0e73e254ea08d3331e014
ReallyRad <arthur.pointeau@gmail.com>
2023-11-22 17:53:34 +0100

    PostProcessing reacting to face tracking in interactive manner. bug with double face expression trigger

--- Assets/Scripts/InteractiveSequenceable.cs
+++ Assets/Scripts/InteractiveSequenceable.cs
@@ -5,7 +5,9 @@ using UnityEngine.Serialization;
 
 public abstract class InteractiveSequenceable : Sequenceable //this abstract class defines the methods that must be implemented by the vfx so they can be sequenced
 {
-   
+    [SerializeField] protected float _riseTime;
+    [SerializeField] protected float _decayTime;
+
     [SerializeField] private FaceData _faceData;
     
     private void OnEnable()
@@ -21,18 +23,19 @@ public abstract class InteractiveSequenceable : Sequenceable //this abstract cla
     
     private void NewFaceExpressionAvailable()
     {
-        if (_faceData.slightSmile)
+        if (_faceData.slightPucker)
         {
-            //reset
-        }
-        else if (_faceData.slightPucker)
-        {
-            //stop
+            Decay();
+            Debug.Log("decay");
         }
         else if (_faceData.pucker)
         {
-            //start
+            Interact();
+            Debug.Log("interact");
         }
     }
+
+    public abstract void Interact();
     
+    public abstract void Decay();
 }
--- Assets/Scripts/PostProcessingControl.cs
+++ Assets/Scripts/PostProcessingControl.cs
@@ -6,275 +6,50 @@ using UnityEngine.Rendering.Universal;
 //using UnityEngine.Rendering.PostProcessing;
 using Unity.VisualScripting;
 
-public class PostProcessingControl : MonoBehaviour
+public class PostProcessingControl : InteractiveSequenceable
 {
-    //public PostProcessProfile postProcessProfile;
-    //public PostProcessVolume postProcessVolume;
-    public VolumeProfile volumeProfile;
-    private void OnEnable()
+    [SerializeField] private Volume _volume;
+    private int _interactTween;
+    private int _decayTween;
+    
+    public override void Initialize()
     {
-        if (ModeControl.Discrete)
-        {
-            if (ModeControl.backgroundChangingRP == "Progressive")
-            {
-                if (ModeControl.DiscreteFB_In_Ex)
-                {
-                    BreathingControl.New_In_Ex_FullBreath += DiscreteProgressiveWhole;
-                }
-                if (ModeControl.DiscreteFB_PeInPi_Ex)
-                {
-                    BreathingControl.New_PeInPi_Ex_FullBreath += DiscreteProgressiveWhole;
-                }
-            }
-            if (ModeControl.backgroundChangingRP == "Interactive")
-            {
-                if (ModeControl.DiscreteFB_In_Ex)
-                {
-                    BreathingControl.New_In_Ex_FullBreath += DiscreteInteractiveWhole;
-                }
-                if (ModeControl.DiscreteFB_PeInPi_Ex)
-                {
-                    BreathingControl.New_PeInPi_Ex_FullBreath += DiscreteInteractiveWhole;
-                }
-            }
-        }
-        if (ModeControl.Continuous)
-        {
-            if (ModeControl.backgroundChangingRP == "Progressive")
-            {
-
-                if (ModeControl.ContinuousACT_Ex)
-                {
-                    Debug.Log("WE ARE AT PROGRESSIVE");
-                    BreathingControl.New_Ex_LongerThanMinimum += ContinuousProgressiveWhole;
-                }
-                if (ModeControl.ContinuousACT_In)
-                {
-                    BreathingControl.New_In_LongerThanMinimum += ContinuousProgressiveWhole;
-                }
-                if (ModeControl.ContinuousACT_PeInPi)
-                {
-                    BreathingControl.New_PeInPi_LongerThanMinimum = ContinuousProgressiveWhole;
-                }
-            }
-            if (ModeControl.backgroundChangingRP == "Interactive")
-            {
-                if (ModeControl.ContinuousACT_Ex)
-                {
-                    Debug.Log("WE ARE AT INTERACTIVE");
-                    BreathingControl.New_Ex_LongerThanMinimum += ContinuousInteractiveWhole;
-                }
-                if (ModeControl.ContinuousACT_In)
-                {
-                    BreathingControl.New_In_LongerThanMinimum += ContinuousInteractiveWhole;
-                }
-                if (ModeControl.ContinuousACT_PeInPi)
-                {
-                    BreathingControl.New_PeInPi_LongerThanMinimum += ContinuousInteractiveWhole;
-                }
-            }
-        }
-    }
-    private void OnDisable()
-    {
-        if (ModeControl.Discrete)
-        {
-            if (ModeControl.auraApproachingRP == "Progressive")
-            {
-                if (ModeControl.DiscreteFB_In_Ex)
-                {
-                    BreathingControl.New_In_Ex_FullBreath -= DiscreteProgressiveWhole;
-                }
-                if (ModeControl.DiscreteFB_PeInPi_Ex)
-                {
-                    BreathingControl.New_PeInPi_Ex_FullBreath -= DiscreteProgressiveWhole;
-                }
-            }
-            if (ModeControl.auraApproachingRP == "Interactive")
-            {
-                if (ModeControl.DiscreteFB_In_Ex)
-                {
-                    BreathingControl.New_In_Ex_FullBreath -= DiscreteInteractiveWhole;
-                }
-                if (ModeControl.DiscreteFB_PeInPi_Ex)
-                {
-                    BreathingControl.New_PeInPi_Ex_FullBreath -= DiscreteInteractiveWhole;
-                }
-            }
-        }
-        if (ModeControl.Continuous)
-        {
-            if (ModeControl.auraApproachingRP == "Progressive")
-            {
-                if (ModeControl.ContinuousACT_Ex)
-                {
-                    BreathingControl.New_Ex_LongerThanMinimum -= ContinuousProgressiveWhole;
-                }
-                if (ModeControl.ContinuousACT_In)
-                {
-                    BreathingControl.New_In_LongerThanMinimum -= ContinuousProgressiveWhole;
-                }
-                if (ModeControl.ContinuousACT_PeInPi)
-                {
-                    BreathingControl.New_PeInPi_LongerThanMinimum -= ContinuousProgressiveWhole;
-                }
-            }
-            if (ModeControl.auraApproachingRP == "Interactive")
-            {
-                if (ModeControl.ContinuousACT_Ex)
-                {
-                    BreathingControl.New_Ex_LongerThanMinimum -= ContinuousInteractiveWhole;
-                }
-                if (ModeControl.ContinuousACT_In)
-                {
-                    BreathingControl.New_In_LongerThanMinimum -= ContinuousInteractiveWhole;
-                }
-                if (ModeControl.ContinuousACT_PeInPi)
-                {
-                    BreathingControl.New_PeInPi_LongerThanMinimum -= ContinuousInteractiveWhole;
-                }
-            }
-        }
-    }
-
-    private void DiscreteProgressiveWhole()
-    {
-        //float exposureChange = AllVfxsControl.backgroundChangingFinalExposure - AllVfxsControl.backgroundChangingInitialExposure;
-        ////float transparencyChange = AllVfxsControl.backgroundChangingFinalTransparency - AllVfxsControl.backgroundChangingInitialTransparency;
-
-        //float colorChangeSteps = AllVfxsControl.backgroundChangingEndBN - AllVfxsControl.backgroundChangingStartBN;
-
-        //float exposureChangeSpeed = exposureChange / colorChangeSteps;
-        //float transarencyChangeSpeed = transparencyChange / colorChangeSteps;
-
-        //if (gameObject.activeSelf)
-        //{
-        //    StartCoroutine(DiscreteProgressiveIncrement(AllVfxsControl.progressIncrementTimeForDiscrete,
-        //                                                exposureChangeSpeed,
-        //                                                transarencyChangeSpeed));
-        //}
-    }
-    private IEnumerator DiscreteProgressiveIncrement(float increment, float exposureSpeed, float transparencySpeed)
-    {
-        float startTime = Time.time;
-        float endTime = startTime + increment;
-
-        while (Time.time < endTime)
-        {
-            //float deltaTime = Time.deltaTime;
-            //float currentExposure = backgroundMat.GetFloat("_Exposure");
-            //backgroundMat.SetFloat("_Exposure", currentExposure + ((exposureSpeed / increment) * deltaTime));
-            //Color color = backgroundMat.color;
-            //color.a += (transparencySpeed / increment) * deltaTime;
-            yield return null;
-        }
-    }
-
-    private void DiscreteInteractiveWhole()
-    {
-        //if (gameObject.activeSelf)
-        //{
-        //    StartCoroutine(DiscreteInteractiveIncrement(AllVfxsControl.interactiveLoopTimeForDiscrete,
-        //                                                AllVfxsControl.backgroundChangingInitialExposure,
-        //                                                AllVfxsControl.backgroundChangingFinalExposure,
-        //                                                AllVfxsControl.backgroundChangingInitialTransparency,
-        //                                                AllVfxsControl.backgroundChangingFinalTransparency));
-        //}
-    }
-    private IEnumerator DiscreteInteractiveIncrement(float increment,
-                                                     float initialExposure,
-                                                     float finalExposure,
-                                                     float initialTransparency,
-                                                     float finalTransparency)
-    {
-        float startTime = Time.time;
-        float middleTime = startTime + (increment / 2);
-        float endTime = startTime + increment;
-
-        while (Time.time < middleTime)
-        {
-            float progress = (middleTime - Time.time) / (increment / 2);
-            float currentExposure = Mathf.Lerp(initialExposure, finalExposure, progress);
-            float currentTransparency = Mathf.Lerp(initialTransparency, finalTransparency, progress);
-
-            //backgroundMat.SetFloat("_Exposure", currentExposure);
-            //Color color = backgroundMat.color;
-            //color.a = currentTransparency;
-
-            yield return null;
-        }
-        while (Time.time > middleTime && Time.time < endTime)
-        {
-            float progress = (endTime - Time.time) / (increment / 2);
-            float currentExposure = Mathf.Lerp(finalExposure, initialExposure, progress);
-            float currentTransparency = Mathf.Lerp(finalTransparency, initialTransparency, progress);
-
-            //backgroundMat.SetFloat("_Exposure", currentExposure);
-            //Color color = backgroundMat.color;
-            //color.a = currentTransparency;
-
-            yield return null;
-        }
-    }
-    //private float currentSaturation;
-    //private float currentExposure;
-    //private float currentContrast;
-    //private float previousSaturation = AllVfxsControl.backgroundChangingInitialSaturation;
-    //private float previousExposure = AllVfxsControl.backgroundChangingInitialExposure;
-    //private float previousContrast = AllVfxsControl.backgroundChangingInitialContrast;
-    private void ContinuousProgressiveWhole(float fraction)
-    {
-        //float exposureChange = AllVfxsControl.backgroundChangingFinalExposure - AllVfxsControl.backgroundChangingInitialExposure;
-        //float saturationChange = AllVfxsControl.backgroundChangingFinalSaturation - AllVfxsControl.backgroundChangingInitialSaturation;
-        //float contrastChange = AllVfxsControl.backgroundChangingFinalContrast - AllVfxsControl.backgroundChangingInitialContrast;
-
-        //float maxIncrementalExposureChange = exposureChange / AllVfxsControl.backgroundChangingNumberOfExhalesIfCompleteToFinish;
-        //float maxIncrementalSaturationChange = saturationChange / AllVfxsControl.backgroundChangingNumberOfExhalesIfCompleteToFinish;
-        //float maxIncrementalContrastChange = contrastChange / AllVfxsControl.backgroundChangingNumberOfExhalesIfCompleteToFinish;
-
-        //float currentExposureIncrement = fraction * maxIncrementalExposureChange * Time.deltaTime;
-        //float currentSaturationIncrement = fraction * maxIncrementalSaturationChange * Time.deltaTime;
-        //float currentContrastIncrement = fraction * maxIncrementalContrastChange * Time.deltaTime;
-
-        //currentExposure = previousExposure + currentExposureIncrement;
-        //currentSaturation = previousSaturation + currentSaturationIncrement;
-        //currentContrast = previousContrast + currentContrastIncrement;
-
-        //if (Mathf.Abs(currentSaturation - AllVfxsControl.backgroundChangingFinalSaturation) > 0.05)
-        //{
-        //    backgroundMat.SetFloat("_Exposure", currentExposure);
-        //    backgroundMat.SetFloat("_Saturation", currentSaturation);
-        //    backgroundMat.SetFloat("_Contrast", currentContrast);
-
-        //    previousExposure = currentExposure;
-        //    previousSaturation = currentSaturation;
-        //    previousContrast = currentContrast;
-        //}
-        //else
-        //{
-        //    gameObject.SetActive(false);
-        //}
-    }
-
-    private void ContinuousInteractiveWhole(float fraction)
-    {
-        float currentSaturation = Mathf.Lerp(AllVfxsControl.postProcessingInitialSaturation,
-                                             AllVfxsControl.postProcessingFinalSaturation,
-                                             fraction);
-        float currentContrast = Mathf.Lerp(AllVfxsControl.postProcessingInitialContrast,
-                                           AllVfxsControl.postProcessingFinalContrast,
-                                           fraction);
-        float currentHueshift = Mathf.Lerp(AllVfxsControl.postProcessingInitialHueshift,
-                                           AllVfxsControl.postProcessingFinalHueshift,
-                                           fraction);
-        ColorAdjustments colorAdjustments;
-        if (volumeProfile.TryGet<ColorAdjustments>(out colorAdjustments))
-        {
-            colorAdjustments.saturation.value = currentSaturation;
-            colorAdjustments.contrast.value = currentContrast;
-            colorAdjustments.hueShift.value = currentHueshift;
-        }
-
+        _active = true;
+        _volume.weight = 0;
         
+    }
+
+    public override void Interact()
+    {
+        if (_decayTween != 0)
+        {
+            LeanTween.pause(_decayTween);
+            Debug.Log("paused decay tween  " + _decayTween);
+        }
+        
+        _interactTween = LeanTween.value(gameObject, _volume.weight, 1, _riseTime)
+            .setOnUpdate(val =>
+            {
+                _volume.weight = val;
+            })
+            .setEaseInCirc()
+            .id;
+    }
+
+    public override void Decay()
+    {
+        if (_interactTween != 0)
+        {
+            LeanTween.pause(_interactTween);
+            Debug.Log("paused interact tween  " + _interactTween);
+        }
+        
+        _decayTween = LeanTween.value(gameObject, _volume.weight, 0, _decayTime)
+            .setOnUpdate(val =>
+            {
+                _volume.weight = val;
+            })
+            .setEaseInCirc()
+            .id;
     }
 }
