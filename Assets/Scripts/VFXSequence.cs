8d0a5d6aaac2090a544495b81402ab7c334e44e4
Arthur Pointeau <>
2024-02-06 20:01:12 +0100

    mute mixer on postprocessing complete

--- Assets/Scripts/VFXSequence.cs
+++ Assets/Scripts/VFXSequence.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.Rendering;
 
-
-public class VFXSequence : MonoBehaviour //takes care of enabling and disabling the different sequenceable vfxs so they can respond to progress one after the other
+public class
+    VFXSequence : MonoBehaviour //takes care of enabling and disabling the different sequenceable vfxs so they can respond to progress one after the other
 {
     [SerializeField] private Sequenceable[] _sequenceableItems;
     [SerializeField] private AudioMixer _mixer;
@@ -14,29 +14,37 @@ public class VFXSequence : MonoBehaviour //takes care of enabling and disabling 
 
     private void OnEnable()
     {
-        Sequenceable.StartNextPhase += SequenceItemCompleted; //disable handler for current element
+        Sequenceable.StartNextPhase += SequenceItemCompleted;
+        PostProcessingControl.StartNextPhase += FirstPhaseCompleted;
     }
 
     private void OnDisable()
     {
-        Sequenceable.StartNextPhase -= SequenceItemCompleted; //disable handler for current element
+        Sequenceable.StartNextPhase -= SequenceItemCompleted; 
+        PostProcessingControl.StartNextPhase -= FirstPhaseCompleted;
+
     }
 
     private void Start()
     {
         _sequenceableItems = transform.GetComponentsInChildren<Sequenceable>(); //get sequenceable items
         _sequenceableItems.First().Initialize(); //initialize it. the first one will now start receiving progress values
+        MuteMixer(_mixer);
+    }
 
-        foreach (AudioMixerGroup group in _mixer.FindMatchingGroups(""))
-        {
-            _mixer.SetFloat(group.name, -80f);
-        }
-        //_mixer.SetFloat("Shimmer", 1f);
+    private void MuteMixer(AudioMixer mixer)
+    {
+        foreach (AudioMixerGroup group in mixer.FindMatchingGroups("")) mixer.SetFloat(group.name, -80f);
     }
 
     private void SequenceItemCompleted(Sequenceable item) //called when current sequence item completed
     {
         sequenceIndex++;
         _sequenceableItems[sequenceIndex].Initialize();
+    }
+
+    private void FirstPhaseCompleted(Sequenceable item)
+    {
+        MuteMixer(_mixer);
     }
 }
