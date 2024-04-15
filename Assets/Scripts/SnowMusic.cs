02c9b5e1005717a6f0e6f0b62d47c88e3f3d514f
MehdiDaemi <53620014+MehdiDaemi@users.noreply.github.com>
2024-04-15 11:04:46 +0200

    make snowmusic and fog progressive sequenceables instead of interactive

--- Assets/Scripts/SnowMusic.cs
+++ Assets/Scripts/SnowMusic.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.Video;
 
-public class SnowMusic : InteractiveSequenceable
+public class SnowMusic : ProgressiveSequenceable
 {
     [SerializeField] private AudioSource _progressiveMusic;
     [SerializeField] private ParticleSystem _snowPS;
@@ -28,48 +28,6 @@ public class SnowMusic : InteractiveSequenceable
         _active = true;
         _localProgress = 0;
     }
-    protected override void Interact()
-    {
-        if (_decayTween != 0)
-        {
-            LeanTween.pause(_decayTween);
-            Debug.Log("paused decay tween  " + _decayTween);
-        }
-
-        float cycleRatio = Utils.Map(_interactiveVal, _initialValue, _finalValue, 0, 1); //calculate the ratio of the current progress
-        float riseTime = (1 - cycleRatio) * _riseTime; //time must be proportional to current progress to keep speed constant
-
-        _interactTween = LeanTween
-            .value(gameObject, _interactiveVal, _finalValue, riseTime)
-            .setOnUpdate(val =>
-            {
-                _interactiveVal = val;
-                TweenHandling(val);
-            })
-            .id;
-    }
-
-    protected override void Decay()
-    {
-        if (_interactTween != 0)
-        {
-            LeanTween.pause(_interactTween);
-            Debug.Log("paused interact tween  " + _interactTween);
-        }
-
-        float cycleRatio = Utils.Map(_interactiveVal, _initialValue, _finalValue, 0, 1);//calculate the ratio of the current progress
-        float decayTime = cycleRatio * _decayTime; //time must be proportional to current progress to keep speed constant
-
-        _decayTween = LeanTween
-            .value(gameObject, _interactiveVal, _initialValue, decayTime)
-            .setOnUpdate(val =>
-            {
-                _interactiveVal = val;
-                TweenHandling(val);
-            })
-            .id;
-    }
-
     protected override void Progress(float progress)
     {
         if (_active)
@@ -97,9 +55,4 @@ public class SnowMusic : InteractiveSequenceable
     }
 
 
-    private void TweenHandling(float val)
-    {
-        //var main = _snowPS.main;
-        //main.simulationSpeed = val;
-    }
 }
