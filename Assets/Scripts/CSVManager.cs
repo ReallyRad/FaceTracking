5384e35eeac3e26acb55bced5dcb29fb9b45c40d
ReallyRad <arthur.pointeau@gmail.com>
2024-04-19 17:49:44 +0200

    basic CSV stuff

---
+++ Assets/Scripts/CSVManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CSVManager : MonoBehaviour
+{
+    [SerializeField] private ExperimentData _experimentData;
+
+    [SerializeField] private List<string> varNames = new List<string>();
+    private List<string> varValues = new List<string>();
+
+    private bool _newFile;
+    
+    private void Start ()
+    {
+        var fields = typeof(ExperimentData).GetFields();
+
+        foreach (var field in fields) {
+            varValues.Add(null); //initialize varNames array
+            varNames.Add(field.Name);
+        }
+
+        _newFile = true;
+    }
+    
+    private void WriteToFile(List<string> stringList)
+    {
+        string stringLine = string.Join(",", stringList.ToArray());
+        string path = "./Logs/" + _experimentData.subjectID + "_log.csv";
+        System.IO.StreamWriter file = new System.IO.StreamWriter(path, true);
+        file.WriteLine(stringLine);
+        file.Close();	
+    }
+
+    public void NewDataAvailable()
+    {
+        if (_newFile) //if this is the first time writing to this CSV, start with column names
+        {
+            WriteToFile(varNames);
+            _newFile = false;
+        }
+        var fields = typeof(ExperimentData).GetFields();
+        for (int i=0; i<fields.Length; i++) //write values from ExperimentData scriptable object to the csv file
+        {
+            varValues[i] = fields[i].GetValue(_experimentData).ToString();
+        }
+        WriteToFile(varValues);
+    }
+}
---
+++ Assets/Scripts/ExperimentData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+public enum QuestionnaireAnswerType {
+    stress,
+    mood,
+    anxiety,
+    happiness
+}
+
+public enum ExperimentState { pre, post }
+
+[CreateAssetMenu]
+public class ExperimentData : ScriptableObject
+{
+    [Header("Experiment Data")]
+    public string subjectID;
+    
+    public QuestionnaireAnswerType answerType;
+    public ExperimentState experimentState;
+
+    public string answerValue;
+
+    public DateTime timestamp;
+}
