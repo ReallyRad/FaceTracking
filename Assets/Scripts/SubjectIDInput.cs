4cd2f2ded0292ffdbbc30154a2a6b08bd00e293c
ReallyRad <arthur.pointeau@gmail.com>
2024-04-29 16:05:40 +0200

    writig data

---
+++ Assets/Scripts/SubjectIDInput.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using ScriptableObjectArchitecture;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SubjectIDInput : MonoBehaviour
+{
+    [SerializeField] private int _modifiedValues;
+    [SerializeField] private int _minLength;
+    [SerializeField] private Button _nextButton;
+
+
+    public void IncrementModifiedValues(string value)
+    {
+        if (value.Length >= _minLength) _nextButton.interactable = true;
+    }
+
+}
