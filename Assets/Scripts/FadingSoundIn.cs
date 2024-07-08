58d0e88309b701b723d7c2fc87da5362aa1e08b2
MehdiDaemi <53620014+MehdiDaemi@users.noreply.github.com>
2024-07-08 13:47:19 +0200

    audio prefabs added + hole working in build

---
+++ Assets/Scripts/FadingSoundIn.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FadingSoundIn : MonoBehaviour
+{
+    [SerializeField] private AudioSource _sound;
+    [SerializeField] private float initialVolume;
+    [SerializeField] private float finalVolume;
+    [SerializeField] private float risingDuration;
+    [SerializeField] private float risingStart;
+
+    private void Start()
+    {
+        _sound.volume = initialVolume;
+        StartCoroutine(StartCoroutineWithDelay());
+    }
+    IEnumerator StartCoroutineWithDelay()
+    {
+        yield return new WaitForSeconds(risingStart);
+        StartCoroutine(SoundFadingIn());
+    }
+    IEnumerator SoundFadingIn()
+    {
+        _sound.volume = initialVolume;
+        float startTime = Time.time;
+
+        while (Time.time < startTime + risingDuration)
+        {
+            _sound.volume = Mathf.Lerp(initialVolume, finalVolume, (Time.time - startTime) / risingDuration);
+            yield return null;
+        }
+        _sound.volume = finalVolume;
+    }
+}
