1e852f23c71425d222accefe56b1bf00185735f0
ReallyRad <arthur.pointeau@gmail.com>
2025-03-10 12:48:35 +0100

    created popup menu that is shown after no breathing was detected for 15 seconds

---
+++ Assets/Scripts/UI/NoBreathingMessage.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class NoBreathingMessage : MonoBehaviour
+{
+    [SerializeField] private int _timeBeforePopup;
+    [SerializeField] private PanelDimmer _panelDimmer;
+
+    private Stopwatch _stopwatch;
+    private bool _shown;
+
+    private void Start()
+    {
+        _stopwatch = new Stopwatch();
+        _panelDimmer.Hide();
+    }
+
+    private void OnEnable()
+    {
+        FaceTrackingManager.PuckerTrigger += PuckerTrigger;
+    }
+
+    private void OnDisable()
+    {
+        FaceTrackingManager.PuckerTrigger -= PuckerTrigger;
+    }
+
+    private void Update()
+    {
+        if (_stopwatch.ElapsedMilliseconds > _timeBeforePopup * 1000 && !_shown) //show panel if too long since last breath
+        {
+            _panelDimmer.Show();
+            _shown = true;
+        }
+    }
+
+    private void PuckerTrigger(bool pucker)
+    {
+        if (!pucker)  //if stopped puckering
+        {
+            _stopwatch.Start(); //stat stopwatch
+        }
+        else
+        {
+            _stopwatch.Stop();
+            _stopwatch.Reset();
+            _panelDimmer.Hide();
+            _shown = false;
+        }
+    }
+    
+}
