b60a6ad75c35de7edefe2339dafa8c4fc6b25a05
ReallyRad <arthur.pointeau@gmail.com>
2025-03-19 17:08:43 +0100

    use convenience scriptable object to end scene and go to post in editor

--- Assets/Scripts/SceneTimer.cs
+++ Assets/Scripts/SceneTimer.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class SceneTimer : MonoBehaviour  //count 10 minutes intervention time and go back to Waiting scene
 {
     [SerializeField] private float _timeToEndScene; 
-    [SerializeField] private ExperimentState _experimentState;
+    [SerializeField] private ExperimentStateSO _experimentState;
 
     public delegate void OnSceneFinished();
     public static OnSceneFinished SceneFinished;
@@ -19,7 +19,12 @@ public class SceneTimer : MonoBehaviour  //count 10 minutes intervention time an
     private IEnumerator StartEndSceneWithDelay()
     {
         yield return new WaitForSeconds(_timeToEndScene);
-        _experimentState = ExperimentState.post;
+        EndScene();
+    }
+
+    public void EndScene()
+    {
+        _experimentState.experimentState = ExperimentState.post;
         SceneFinished();
         SceneManager.LoadScene("Waiting");
     }
