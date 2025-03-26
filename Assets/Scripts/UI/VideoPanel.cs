3b2f85c041b78edb6d931aded4df85b3dcfba9ab
ReallyRad <arthur.pointeau@gmail.com>
2025-03-26 11:15:01 +0100

    hide video panel on video complete.

--- Assets/Scripts/UI/VideoPanel.cs
+++ Assets/Scripts/UI/VideoPanel.cs
@@ -8,14 +8,11 @@ public class VideoPanel : MonoBehaviour
     [SerializeField] private string _videoFileName;
     [SerializeField] private Button _nextButton;
 
-    public delegate void OnVideoCompleted();
-    public static OnVideoCompleted VideoCompleted;
-
     private void Start()
     {
         string videoPath = System.IO.Path.Combine(Application.persistentDataPath, _videoFileName);
         if (System.IO.File.Exists(videoPath)) _videoPlayer.url = videoPath;
-        _videoPlayer.loopPointReached += source => _nextButton.interactable = true;
+        _videoPlayer.loopPointReached += source => GetComponent<PanelDimmer>().Show(false);
     }
 
     public void PlayVideo()
